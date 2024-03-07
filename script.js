04f3d2935bf1d17fdfbc29561a2ec803b893ab38
sofilarrea <sofialejandra.larrea@gmail.com>
2024-03-07 19:29:36 -0300

    adding color

--- script.js
+++ script.js
@@ -3,23 +3,39 @@
 //             Hacer un programa que al iniciarse pida mediante un prompt ingresar un nombre, y una vez ingresado muestre un saludo con el nombre dentro de un h1. El h1 debe estar centrado y tener un tamaño de letra y una tipografía distinta de la que viene por defecto-
 
 
-document.addEventListener("DOMContentLoaded", function(){
-    let nombreUsuario = prompt("Por favor, ingresá tu nombre:");
-    if (nombreUsuario !== null && nombreUsuario != "") {
-        alert.saludo(nombreUsuario)
-    } else {
-        alert("INGRESA ALGO VALIDO")
-    }
-});
+// document.addEventListener("DOMContentLoaded", function(){
+//     let nombreUsuario = prompt("Por favor, ingresá tu nombre:");
+//     if (nombreUsuario !== null && nombreUsuario != "") {
+//         alert.saludo(nombreUsuario)
+//     } else {
+//         alert("INGRESA ALGO VALIDO")
+//     }
+// });
 
-function saludo(nombreUsuario){
-    let h1 = document.createElement("h1");
-    h1.textContent = "¡Hola, " + nombre + "!";
-    h1.style.textAlign = center;
-    h1.style.fontFamily = "Roboto, san-serif"
-    h1.style.fontFamily = "24px";
-    document.body.appendChild(h1);
-}
+// function saludo(nombreUsuario){
+//     let h1 = document.createElement("h1");
+//     h1.textContent = "¡Hola, " + nombre + "!";
+//     h1.style.textAlign = center;
+//     h1.style.fontFamily = "Roboto, san-serif"
+//     h1.style.fontFamily = "24px";
+//     document.body.appendChild(h1);
+// }
 
 
-let button = document.getElementById
+// let button = document.getElementById
+
+
+// color
+// Hacer un programa que al iniciarse pida mediante un prompt ingresar un color en sistema hexadecimal, y una vez ingresado ponga el color de fondo del body con dicho color.
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     let colorHexadecimal = prompt("Por favor ingresa tu color favorito!")
+//     if(colorHexadecimal !== null && colorHexadecimal !== ""){
+//         cambiarColorDeFondo(colorHexadecimal)
+//     } else {
+//         "Ingresá un color valido"
+//     }
+// });
+// function cambiarColorDeFondo(colorHexadecimal){
+//     document.body.style.backgroundColor = colorHexadecimal;
+// }
